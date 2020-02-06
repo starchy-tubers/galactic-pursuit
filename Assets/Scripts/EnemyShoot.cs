1fcc7d4c0c6b4d1bf312804a4b714a2fe60c5d7f
Raj Patel <55072710+rajnpatel@users.noreply.github.com>
2020-02-05 19:59:54 -0600

    Enabled enemies to shoot

---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject EnemyBullet;
+    public float delayTime = 2.0f;
+    bool canShoot = true;
+
+    private void Update()
+    {
+        if (!canShoot) return;
+        canShoot = false;
+        Instantiate(EnemyBullet, transform.position, transform.rotation);
+        StartCoroutine(NoFire());
+    }
+
+    private IEnumerator NoFire()
+    {
+        yield return new WaitForSeconds(delayTime);
+        canShoot = true;
+    }
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -9,6 +9,14 @@ public class Ship : MonoBehaviour
         fixedScroll = (FixedScroll)FindObjectOfType(typeof(FixedScroll));
     }
 
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("EnemyBullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void Update()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + fixedScroll.theScrollSpeed);
