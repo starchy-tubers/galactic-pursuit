dca4be7c6f4c90bd7a3f59e6eebdb429e99e6ffa
Raj Patel <55072710+rajnpatel@users.noreply.github.com>
2020-02-08 16:11:31 -0600

    Made enemies light up when hit

---
+++ Assets/Scripts/Enemy/EnemyHurt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHurt : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public float delayTime;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+            spriteRenderer.sprite = sprite1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("ShipBullet"))
+        {
+            ChangeSprite();
+            StartCoroutine(SpriteDelay());
+        }
+    }
+
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = sprite2;
+    }
+    private IEnumerator SpriteDelay()
+    {
+        yield return new WaitForSeconds(delayTime);
+        ChangeSprite2();
+    }
+    void ChangeSprite2()
+    {
+        spriteRenderer.sprite = sprite1;
+    }
+}
