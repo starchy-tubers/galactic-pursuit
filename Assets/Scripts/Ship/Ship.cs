0885b877acb5a9593337413d04c094ec28d0a959
Raj Patel <55072710+rajnpatel@users.noreply.github.com>
2020-02-13 21:46:58 -0600

    Added health bar for player ship

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Ship : MonoBehaviour
 {
     public int shipHealth = 3;
-
-    private void OnCollisionEnter2D(Collision2D col)
+    public float delayTime = 0.01f;
+   
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (col.gameObject.CompareTag("EnemyBullet"))
+        if (collision.gameObject.CompareTag("EnemyBullet"))
         {
             shipHealth -= 1;
-
-            if (shipHealth == 0) {
-
-                Destroy(gameObject);
+            if (shipHealth == 0) 
+            {
+             StartCoroutine(destroy());
             }
         }
     }
+    private IEnumerator destroy()
+        {
+            yield return new WaitForSeconds(delayTime);
+            Destroy(gameObject);
+        }
 }
