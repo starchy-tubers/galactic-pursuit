aef92134544bd483d7edf5700394992b1b0a6540
Raj Patel <55072710+rajnpatel@users.noreply.github.com>
2020-02-15 17:27:28 -0600

    Add-Health-Bar-to-Indicate-Player's-Current-Health

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -4,22 +4,38 @@ using System.Collections;
 public class Ship : MonoBehaviour
 {
     public int shipHealth = 3;
-    public float delayTime = 0.01f;
-   
+    public Sprite healthBar3;
+    public Sprite healthBar2;
+    public Sprite healthBar1;
+    public Sprite healthBar0;
+    private SpriteRenderer spriteRenderer;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("EnemyBullet"))
         {
+            spriteRenderer = GameObject.FindWithTag("HealthBar").GetComponent<SpriteRenderer>();
             shipHealth -= 1;
+
+            switch (shipHealth)
+            {
+                case 3:
+                    spriteRenderer.sprite = healthBar3;
+                    break;
+                case 2:
+                    spriteRenderer.sprite = healthBar2;
+                    break;
+                case 1:
+                    spriteRenderer.sprite = healthBar1;
+                    break;
+                default:
+                    spriteRenderer.sprite = healthBar0;
+                    break;
+            }
+
             if (shipHealth == 0) 
             {
-             StartCoroutine(destroy());
+                Destroy(gameObject);
             }
         }
     }
-    private IEnumerator destroy()
-        {
-            yield return new WaitForSeconds(delayTime);
-            Destroy(gameObject);
-        }
 }
