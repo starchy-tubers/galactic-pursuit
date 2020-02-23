a4f56b6bf41a7e02d405d2df97d6d4bf81842655
Raj Patel <55072710+rajnpatel@users.noreply.github.com>
2020-02-22 21:46:28 -0600

    Made a separate script for instantiating asteroids for SceneManager

--- Assets/Scripts/Asteroid/AsteroidAppear.cs
+++ Assets/Scripts/Asteroid/AsteroidAppear.cs
@@ -4,43 +4,15 @@ using UnityEngine;
 
 public class AsteroidAppear : MonoBehaviour
 {
-    public GameObject Asteroid;
-    public GameObject BulletsPowerUp;
     bool canAppear = true;
     float RandomNum;
-    private const float velX = 0;
-    public float velY = -1f;
-    Rigidbody2D rb;
-    int asteroidHealth = 10;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("ShipBullet"))
-        {
-            asteroidHealth -= 1;
-            if (asteroidHealth == 0)
-            {
-                Destroy(gameObject);
-                if (Random.Range(0.00f, 1.00f) < 0.25)
-                {
-                    Instantiate(BulletsPowerUp, transform.position, transform.rotation);
-                }
-            }
-        }
-    }
-
+    public GameObject Asteroid;
     private void Update()
     {
         RandomNum = Random.Range(-2.4f, 2.4f);
         if (!canAppear) return;
         canAppear = false;
         StartCoroutine(noAppear());
-        rb.velocity = new Vector2(velX, velY);
     }
 
     private IEnumerator noAppear()
@@ -49,9 +21,10 @@ public class AsteroidAppear : MonoBehaviour
         Instantiate(Asteroid, new Vector2(RandomNum, 5), transform.rotation);
         canAppear = true;
     }
-    
+
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
     }
+
 }
