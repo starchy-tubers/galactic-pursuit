52ee0cae9ab6bf0622664130ebb1fda92113cb9d
Adam L. Bunch <adam@adamlbunch.com>
2020-02-26 08:03:32 -0600

    Added new healthbar and updated enemy bullet

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 public class Ship : MonoBehaviour
 {
-    public int shipHealth = 3;
-    public Sprite[] spriteList;
+    [SerializeField]
+    int shipHealth = 10;
+    [SerializeField]
+    Sprite[] healthBarSpriteArray;
     private SpriteRenderer spriteRenderer;
     bool canDamage = true;
 
@@ -15,24 +17,49 @@ public class Ship : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
+        // TODO: Need to make a list of things that can damage the ship and check if the gameObject exists in that list
+        // This if statement is unsustainable as more hostile objects are added
         if (col.gameObject.CompareTag("EnemyBullet") || col.gameObject.CompareTag("GreenEnemy") || col.gameObject.CompareTag("Asteroid"))
         {
-            if (!canDamage) return;
-            Damage();
-            canDamage = false;
+            // if (!canDamage) return;
+            shipHealth -= 1;
+            Debug.Log(shipHealth);
+            // canDamage = false;
+            // TODO: Probably a smarter way to do this
             switch (shipHealth)
             {
+                case 10:
+                    spriteRenderer.sprite = healthBarSpriteArray[10];
+                    break;
+                case 9:
+                    spriteRenderer.sprite = healthBarSpriteArray[9];
+                    break;
+                case 8:
+                    spriteRenderer.sprite = healthBarSpriteArray[8];
+                    break;
+                case 7:
+                    spriteRenderer.sprite = healthBarSpriteArray[7];
+                    break;
+                case 6:
+                    spriteRenderer.sprite = healthBarSpriteArray[6];
+                    break;
+                case 5:
+                    spriteRenderer.sprite = healthBarSpriteArray[5];
+                    break;
+                case 4:
+                    spriteRenderer.sprite = healthBarSpriteArray[4];
+                    break;
                 case 3:
-                    spriteRenderer.sprite = spriteList[3];
+                    spriteRenderer.sprite = healthBarSpriteArray[3];
                     break;
                 case 2:
-                    spriteRenderer.sprite = spriteList[2];
+                    spriteRenderer.sprite = healthBarSpriteArray[2];
                     break;
                 case 1:
-                    spriteRenderer.sprite = spriteList[1];
+                    spriteRenderer.sprite = healthBarSpriteArray[1];
                     break;
                 default:
-                    spriteRenderer.sprite = spriteList[0];
+                    spriteRenderer.sprite = healthBarSpriteArray[0];
                     break;
             }
 
