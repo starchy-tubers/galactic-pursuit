53cfb7584ac30fcffdcfc6512ebaf0317527208a
Adam L. Bunch <adam@adamlbunch.com>
2020-03-01 09:58:38 -0600

    Changed stuff

--- Assets/Scripts/Ship Bullet/ShipBullet.cs
+++ Assets/Scripts/Ship Bullet/ShipBullet.cs
@@ -2,7 +2,7 @@
 
 public class ShipBullet : MonoBehaviour
 {
-    public float velY = 5f;
+    public float speed = 5f;
     private const float velX = 0;
     Rigidbody2D rb;
 
@@ -10,15 +10,15 @@ public class ShipBullet : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    
+
     private void Update()
     {
-        rb.velocity = new Vector2(velX, velY);
+        rb.velocity = new Vector2(velX, speed);
     }
-            
+
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("GreenEnemy")|| col.gameObject.CompareTag("Asteroid"))
+        if (col.gameObject.CompareTag("GreenEnemy") || col.gameObject.CompareTag("Asteroid"))
         {
             Destroy(gameObject);
         }
